2ef78541555fb8f6d130ff5373461c873b1709d8
yaredghebre <yared2391@gmail.com>
2023-11-23 14:54:57 +0100

    added routers (defined) starting controllers

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const postsRouter = require("./routers/postsRouter");
 
 const app = express();
 let port = +process.env.PORT || 3001;
 
 dotenv.config();
+
+app.use(express.json());
+
+app.use("/posts", postsRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on http:/localhost:${port}`);
---
+++ routers/postsRouter.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+const postsController = require("../controllers/postsController");
+
+// Definisco rotte per le operazioni CRUD
+
+router.get("/", postsController.index);
+
+router.get("/:slug", postsController.show);
+
+router.post("/", postsController.store);
+
+router.put("/", postsController.update);
+
+router.delete("/:slug", postsController.destroy);
+
+module.exports = router;
